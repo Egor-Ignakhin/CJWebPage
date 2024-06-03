184fdf253cce002acb04553a23875804cb778e5b
Valentin Nazarenko <dxoraxslife@gmail.com>
2024-06-03 14:04:05 +0500

    upd

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-mario_project-1.03";
 const contentToCache = [
 "Build/JOTCMC2QL33JG78.loader.js,",
-"Build/NY6OGVXGH5SVS5L.framework.js.unityweb",
-"Build/NY6OGVXGH5SVS5L.data.unityweb",
-"Build/NY6OGVXGH5SVS5L.wasm.unityweb",
+"Build/OYSQM4CB6ALOHDW.framework.js.unityweb",
+"Build/OYSQM4CB6ALOHDW.data.unityweb",
+"Build/OYSQM4CB6ALOHDW.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
